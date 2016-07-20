97034c054b05e8219693c75c1efd79bdf18c93a5
jiangg <jiangg@mail.ustc.edu.cn>
2016-07-20 23:21:34 +0800

    Add install & uninstall scripts in Linux

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -x
+
+sudo ln -sfr ip-pre-up /etc/ppp/
+sudo ln -sfr ip-down /etc/ppp/ip-down.d/
---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -x
+
+sudo rm -f /etc/ppp/ip-pre-up /etc/ppp/ip-down.d/ip-down
